72bb04543c657341bbbde94753a8ecaa04c46138
Artem Marhaza <artemmrgz@gmail.com>
2023-05-22 18:32:52 +0200

    create empty project

---
+++ HabitTracker/HabitTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HabitTracker
+//
+//  Created by Artem Marhaza on 22/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
